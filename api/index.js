7b342a4bd944768c8ece9b1183e5d2169e1edb55
Shaquille Arriza Hidayat <shaquirearija1@gmail.com>
2024-10-22 08:29:30 +0000

    Create database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.DB_URI)
+    .then(() => {
+        console.log("Database connected!");
+    })
+    .catch((error) => {
+        console.error("Error: " + error);
+    });
 
 const app = express();
 
 app.listen(5000, () => {
-    console.log("Server is running on port 5000!!");
+    console.log("Server is running on port 5000!");
 });

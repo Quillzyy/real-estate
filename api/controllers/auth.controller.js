4b3be51408e41d3b1eba21a1ee1fc0f4d780e8ea
Shaquille Arriza Hidayat <shaquirearija1@gmail.com>
2024-10-23 18:08:46 +0000

    Complete UI for sign in page and fix sign up handling error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,21 +6,30 @@ import jwt from "jsonwebtoken";
 // Callback for /models/auth.route.js
 
 export async function signup(req, res, next) {
-    const { username, email, password } = req.body;
-
-    // Scrypt
-
-    const salt = randomBytes(16).toString("hex");
-    const saltWord = scryptSync(password, salt, 64).toString("hex") + salt;
-    const encodeWord = Buffer.from(saltWord).toString("base64");
-    // console.log("saltword:", saltword);
-    // console.log("encodeword:", encodeword);
-    // const decodeword = Buffer.from(encodeword, "base64").toString();
-    // const salt = encodeword.slice(-32);
-    // const hashword = encodeword.slice(0, -32);
-
-    const newUser = new User({ username, email, password: encodeWord });
     try {
+        const { username, email, password } = req.body;
+
+        // Check if username or email already exists
+        const validUser = await User.findOne({
+            $or: [{ username }, { email }],
+        });
+        // console.log(validUser);
+        if (validUser) {
+            if (validUser.username === username) {
+                return next(errorHandler(400, "Username already exists!"));
+            } else if (validUser.email === email) {
+                return next(errorHandler(400, "Email already exists!"));
+            }
+        }
+
+        // Scrypt
+        const salt = randomBytes(16).toString("hex");
+        const saltWord = scryptSync(password, salt, 64).toString("hex") + salt;
+        const encodeWord = Buffer.from(saltWord).toString("base64");
+
+        // Create new user
+        const newUser = new User({ username, email, password: encodeWord });
+        // console.log(newUser);
         await newUser.save();
         res.status(201).json({ message: "User created successfully!" });
     } catch (err) {
@@ -34,6 +43,7 @@ export async function signin(req, res, next) {
             return next(errorHandler(400, "Email and password are required!"));
         }
 
+        // Check if user exists
         const { email, password } = req.body;
         const validUser = await User.findOne({ email });
         if (!validUser) {
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignIn() {
-    return <div>SignIn</div>;
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+    });
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    }
+    async function handleSubmit(e) {
+        try {
+            e.preventDefault();
+            setLoading(true);
+            const res = await fetch("/api/auth/signin", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                setError("Error: " + data.message);
+                setLoading(false);
+                return;
+            }
+            setLoading(false);
+            navigate("/");
+        } catch (error) {
+            setError("Error: " + error.message);
+            setLoading(false);
+        }
+    }
+    return (
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="email"
+                    className="border p-3 rounded-lg"
+                    id="email"
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type="password"
+                    placeholder="password"
+                    className="border p-3 rounded-lg"
+                    id="password"
+                    onChange={handleChange}
+                ></input>
+                <button
+                    disabled={
+                        loading ||
+                        formData.email === "" ||
+                        formData.password === ""
+                    }
+                    className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+                >
+                    {loading ? "Loading..." : "Sign Up"}
+                </button>
+            </form>
+            <div className="flex flex-row gap-2 mt-5">
+                <p>Don&apos;t have an account?</p>
+                <Link to={"/sign-up"}>
+                    <span className="text-blue-700">Sign up</span>
+                </Link>
+            </div>
+            {error && <p className="text-red-500 mt-5">{error}</p>}
+        </div>
+    );
 }
 
 export default SignIn;
